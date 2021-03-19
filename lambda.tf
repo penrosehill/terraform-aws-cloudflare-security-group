67217ecb0b1715a6ce53ae7914db401307dd55a9
gugaiz <gugaiz@yahoo.com.ar>
2021-03-19 10:40:09 -0300

    terraform 0.13 compatibility

--- lambda.tf
+++ lambda.tf
@@ -1,10 +1,10 @@
 resource "aws_cloudwatch_log_group" "lambda-log-group" {
   name  = "UpdateCloudflareIps"
-  count = "${var.enabled == "true" ? 1 : 0}"
+  count = var.enabled ? 1 : 0
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
-  count              = "${var.enabled == "true" ? 1 : 0}"
+  count              = var.enabled ? 1 : 0
   name               = "lambda-cloudflare-role"
   assume_role_policy = <<EOF
 {
@@ -24,10 +24,10 @@ EOF
 }
 
 resource "aws_iam_policy" "policy" {
-  name = "lambda-cloudflare-policy"
-  count = "${var.enabled == "true" ? 1 : 0}"
+  name        = "lambda-cloudflare-policy"
+  count       = var.enabled ? 1 : 0
   description = "Allows cloudflare ip updating lambda to change security groups"
-  policy = <<EOF
+  policy      = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -61,30 +61,30 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "policy" {
-  count      = "${var.enabled == "true" ? 1 : 0}"
-  role       = "${aws_iam_role.iam_for_lambda[count.index].id}"
-  policy_arn = "${aws_iam_policy.policy[count.index].arn}"
+  count      = var.enabled ? 1 : 0
+  role       = aws_iam_role.iam_for_lambda[count.index].id
+  policy_arn = aws_iam_policy.policy[count.index].arn
 }
 
 data "archive_file" "lambda_zip" {
-  count       = "${var.enabled == "true" ? 1 : 0}"
+  count       = var.enabled ? 1 : 0
   type        = "zip"
   source_file = "${path.module}/cloudflare-security-group.py"
   output_path = "${path.module}/lambda.zip"
 }
 
 resource "aws_lambda_function" "update-ips" {
-  count            = "${var.enabled == "true" ? 1 : 0}"
+  count            = var.enabled ? 1 : 0
   function_name    = "UpdateCloudflareIps"
   filename         = "${path.module}/lambda.zip"
-  source_code_hash = "${data.archive_file.lambda_zip[count.index].output_base64sha256}"
+  source_code_hash = data.archive_file.lambda_zip[count.index].output_base64sha256
   handler          = "cloudflare-security-group.lambda_handler"
-  role             = "${aws_iam_role.iam_for_lambda[count.index].arn}"
+  role             = aws_iam_role.iam_for_lambda[count.index].arn
   runtime          = "python3.6"
   timeout          = 60
   environment {
     variables = {
-      SECURITY_GROUP_ID = "${var.security_group_id}"
+      SECURITY_GROUP_ID = var.security_group_id
     }
   }
 }
