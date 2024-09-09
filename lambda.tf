ebadd1c7137a6d732b39e99ea01102a0182b6786
gugaiz <gugaiz@yahoo.com.ar>
2024-09-09 12:44:04 -0300

    update python version

--- lambda.tf
+++ lambda.tf
@@ -81,7 +81,7 @@ resource "aws_lambda_function" "update-ips" {
   source_code_hash = data.archive_file.lambda_zip[count.index].output_base64sha256
   handler          = "cloudflare-security-group.lambda_handler"
   role             = aws_iam_role.iam_for_lambda[count.index].arn
-  runtime          = "python3.9"
+  runtime          = "python3.12"
   timeout          = 60
   environment {
     variables = {
